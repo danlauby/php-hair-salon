b2e21e1cf759e17938991546f00a23845e210288
danlauby <dmlauby@gmail.com>
2017-02-24 08:53:44 -0800

    Create empty methods in Stylist class and link to StylistTest

--- src/Stylist.php
+++ src/Stylist.php
@@ -1,1 +1,58 @@
-<?php  ?>
+<?php
+    class Stylist
+    {
+        private $stylist_name;
+        private $id;
+
+        function __construct($stylist_name, $id = null)
+        {
+            $this->stylist_name = $stylist_name;
+            $this->id = $id;
+        }
+
+        function setStylistName()
+        {
+
+        }
+
+        function getStylistName()
+        {
+
+        }
+
+        function getId()
+        {
+
+        }
+
+        function save()
+        {
+
+        }
+
+        function update()
+        {
+
+        }
+
+        function delete()
+        {
+
+        }
+
+        static function getAll()
+        {
+
+        }
+
+        static function deleteAll()
+        {
+
+        }
+
+        static function find()
+        {
+
+        }
+    }
+ ?>
